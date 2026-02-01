63ac82a9a0b4111c9d3373414f9e59c4a6467e45
hmidox <hmidoxaboudrar5751@gmail.com>
2026-02-01 01:54:58 +0100

    I changed the sound of the "Sound Block" from NOTE_BLOCK_BELL to RESPAWN_ANCHOR_CHARGE

--- src/main/java/net/hmidox/firstmod/block/custom/SoundBlock.java
+++ src/main/java/net/hmidox/firstmod/block/custom/SoundBlock.java
@@ -26,7 +26,7 @@ public class SoundBlock extends Block {
     @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
 
-        pLevel.playSound(pPlayer, pPos, SoundEvents.NOTE_BLOCK_BELL.get(), SoundSource.BLOCKS, 1f, 1f);
+        pLevel.playSound(pPlayer, pPos, SoundEvents.RESPAWN_ANCHOR_CHARGE, SoundSource.BLOCKS, 1f, 1f);
         return InteractionResult.SUCCESS;
 
     }
